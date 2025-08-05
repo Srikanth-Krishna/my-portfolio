c6fcfde70275b91c3a96ad19d6d5a71d632bc470
Srikanth-Krishna <srikanth.krishna1398@gmail.com>
2025-08-05 20:53:18 +0530

    Tweaked form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -12,7 +12,7 @@ export default function ContactPage() {
         Interested in working together or just want to say hi? Drop me a message
         below.
       </p>
-      <form className='contact-form-dark'>
+      <form className='contact-form-dark' name='contact' netlify>
         <input type='text' name='name' placeholder='Your Name' required />
         <input type='email' name='email' placeholder='Your Email' required />
         <textarea name='message' rows='6' placeholder='Your Message' required />
